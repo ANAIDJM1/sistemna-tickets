73b02b8f397bb83d33bce916afe87ba230b71689
anaidjm1 <anaidjm1@gmail.com>
2020-12-11 14:11:30 -0500

    subida

---
+++ reportes/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">      
+        <h1>
+          Sistema Reportes Ticket por dia
+        </h1>
+        <label>Ingreese area:</label>
+        <input></input>
+        <label>Ingrese Password:</label>    
+        <input></input>   
+      </header>
+    </div>
+  );
+}
+
+export default App;
